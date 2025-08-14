c24f41c3e563cdd291d5e5c9bface59d49951de7
ChoKJ <siregy72@gmail.com>
2025-08-14 14:23:12 +0900

    1

---
+++ resources/lang/ko/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+ /*
+ |--------------------------------------------------------------------------
+ | Authentication Language Lines
+ |--------------------------------------------------------------------------
+ |
+ | The following language lines are used during authentication for various
+ | messages that we need to display to the user. You are free to modify
+ | these language lines according to your application's requirements.
+ |
+ */
+
+ 'failed' => '제공된 인증 정보가 기록과 일치하지 않습니다.',
+ 'password' => '비밀번호가 올바르지 않습니다.',
+ 'throttle' => '너무 많은 로그인 시도가 있었습니다. :seconds초 후에 다시 시도해주세요.',
+
+];
---
+++ resources/lang/ko/validation.php
@@ -0,0 +1,57 @@
+ge Lines
+ |--------------------------------------------------------------------------
+ |
+ | Here you may specify custom validation messages for attributes using the
+ | convention "rule.attribute" to name the lines. This makes it quick to
+ | specify a specific custom language line for a given attribute rule.
+ |
+ */
+
+ 'custom' => [
+  'attribute-name' => [
+   'rule-name' => 'custom-message',
+  ],
+ ],
+
+ /*
+ |--------------------------------------------------------------------------
+ | Custom Validation Attributes
+ |--------------------------------------------------------------------------
+ |
+ | The following language lines are used to swap our attribute placeholder
+ | with something more reader friendly such as "E-Mail Address" instead
+ | of "email". This simply helps us make our message more expressive.
+ |
+ */
+
+ 'attributes' => [
+  'name' => '이름',
+  'username' => '사용자명',
+  'email' => '이메일',
+  'password' => '비밀번호',
+  'password_confirmation' => '비밀번호 확인',
+  'city' => '도시',
+  'country' => '국가',
+  'address' => '주소',
+  'phone' => '전화번호',
+  'mobile' => '휴대전화',
+  'age' => '나이',
+  'sex' => '성별',
+  'gender' => '성별',
+  'day' => '일',
+  'month' => '월',
+  'year' => '년',
+  'hour' => '시간',
+  'minute' => '분',
+  'second' => '초',
+  'title' => '제목',
+  'content' => '내용',
+  'description' => '설명',
+  'excerpt' => '요약',
+  'date' => '날짜',
+  'time' => '시간',
+  'available' => '사용 가능',
+  'size' => '크기',
+ ],
+
+];
